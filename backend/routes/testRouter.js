ae16a773d31c0b533e91d43403adfd67717dd864
Batsen <94564906+Sibriaev-Egor@users.noreply.github.com>
2024-05-01 20:07:41 +0400

    Delete backend directory

--- backend/routes/testRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const testController = require('../controllers/testController');
-
-router.get('/test_all', testController.get_all_user);
-router.get('/getstatuser', testController.get_static_user);
-
-
-module.exports = router;
